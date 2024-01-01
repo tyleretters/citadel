49142cd26824915ed0b21963e04050f515db8744
Tyler Etters <tyler@etters.co>
2023-12-31 16:49:36 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Citadel
+
+An interactive experience for the discography of Tyler Etters.
+
+## Setup Instructions
+
+- clone citadel
+- `cd citadel`
+- `npm start`
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Citadel</h1>
+      <em>An Interactive Discography of Tyler Etters</em>
+    </div>
+  );
+}
+
+export default App;
