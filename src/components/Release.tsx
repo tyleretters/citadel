10a381f72c625c07686090147270775c370069b9
Tyler Etters <tyler@etters.co>
2024-01-02 15:37:08 -0800

    images!

--- src/components/Release.tsx
+++ src/components/Release.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import type { ReleaseType } from '../types'
+import { Release as ReleaseType } from '@tyleretters/discography'
 
 function Release(data: { data: ReleaseType }) {
   const release = data.data // todo
+  const bucket = 'https://intertext.s3.us-west-1.amazonaws.com/'
+  const project_slug = release.project_slug
+  const release_slug = release.release_slug
+  const imgSrc = `${bucket}${project_slug}/${release.release_slug}/${release_slug}.jpg`
   return (
     <div className="release">
       <h1>{ release.title }<span>{ release.released }</span></h1>
+      <img src={ imgSrc } alt={ release.title } />
       <p>By: { release.project }</p>
       <ol>
       { release.tracks.map((track, i) => (
