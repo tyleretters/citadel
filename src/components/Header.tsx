dcc57192d110c97e72d78ad4def9d5c9de759b23
Tyler Etters <tyler@etters.co>
2024-01-04 15:02:00 -0800

    tighten up header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,11 @@ function Header() {
     <div className="header">
       <h1>Discography of Tyler Etters</h1>
       <p>
-        <Pipe className="d-none d-md-inline-block" />
-        <a href="https://paypal.me/tyleretters">Donate</a>
-        <Pipe />
         <a href="https://github.com/tyleretters/discography-frontend">GitHub</a>
         <Pipe />
         <a href="https://www.npmjs.com/package/@tyleretters/discography">NPM</a>
+        <Pipe />
+        <a href="https://paypal.me/tyleretters">Donate</a>
         <Pipe />
         <a href="https://nor.the-rn.info">Blog</a>
         <Pipe />
