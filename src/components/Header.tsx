ea0a5dbf0289f40707bb03ab1bcb6133f5423755
Tyler Etters <tyler@etters.co>
2024-04-23 08:54:18 -0700

    display discography version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,24 @@
-import React from "react"
-import Pipe from "./Pipe"
+import React, { useEffect, useState } from 'react'
+import Pipe from './Pipe'
+import packageJson from '../../package.json'
 
 export default function Header() {
+  const [version, setVersion] = useState('')
+
+  useEffect(() => {
+    const discographyVersion = packageJson.dependencies['@tyleretters/discography']
+    setVersion(discographyVersion)
+  }, [])
+
   return (
     <div className="header">
       <h1>Discography of Tyler Etters</h1>
       <p>
         <a href="https://github.com/tyleretters/discography-frontend">GitHub</a>
         <Pipe />
-        <a href="https://www.npmjs.com/package/@tyleretters/discography">NPM</a>
+        <a href="https://www.npmjs.com/package/@tyleretters/discography">
+          {version ? `NPM v${version}` : 'NPM'}
+        </a>
         <Pipe />
         <a href="https://paypal.me/tyleretters">Donate</a>
         <Pipe />
